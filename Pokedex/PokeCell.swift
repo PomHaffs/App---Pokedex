74939ae00e35e99e7fe9d7b79d55bb80bad2f42a
PomHaffs <tomashaffenden@icloud.com>
2016-11-11 16:13:44 +1100

    added custom view cell and logic

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Tomas-William Haffenden on 11/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+ 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
