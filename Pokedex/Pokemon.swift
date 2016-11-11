568fde20c3ff1655768f35c7898273e16c1a0230
PomHaffs <tomashaffenden@icloud.com>
2016-11-11 14:30:57 +1100

    basic init for pokedex added

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,38 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Tomas-William Haffenden on 11/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        if _name == nil {
+            _name = ""
+        }
+        return _name
+    }
+    
+    var pokedexId: Int {
+        if _pokedexId == nil {
+            _pokedexId = 0
+        }
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+}
