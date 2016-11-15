f24fe57c41e7819a7874ca7f38f21bbec96a5d54
PomHaffs <tomashaffenden@icloud.com>
2016-11-15 13:11:52 +1100

    all IBOutlets hooked up

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -13,13 +13,29 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenceLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name
         
     }
-
+    
+//This is full functionality for back button
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
