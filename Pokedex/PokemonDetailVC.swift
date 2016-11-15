3313923d4d41684eeb502e892778926dd55948fa
PomHaffs <tomashaffenden@icloud.com>
2016-11-15 12:17:58 +1100

    added mock up to details view

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -17,7 +17,7 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = pokemon.name
+        //nameLabel.text = pokemon.name
         
     }
 
